a9ff6073239d6c0a4fc20ab5e5bc2dc4b0c9df91
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2018-05-06 22:09:12 +0200

    Added new screenshot example. Fixed Number serialisation.

--- sources/Node_Number.cpp
+++ sources/Node_Number.cpp
@@ -26,8 +26,8 @@ std::string Node_Number::getValueAsString()const
 {
 	// Format the num as a string without any useless ending zeros/dot
 	std::string str = std::to_string (this->value);
-	str.erase ( str.find_last_not_of('0'), std::string::npos );
-	if (str.find_last_of('.') + 1 == std::string::npos)
+	str.erase ( str.find_last_not_of('0') +1, std::string::npos );
+	if (str.find_last_of('.') +1 == str.size())
 		str.erase ( str.find_last_of('.'), std::string::npos );
 	return str;
 }
