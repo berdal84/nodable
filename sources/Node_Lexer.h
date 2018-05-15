dae38d44cfba96d25b49f3a368266263d3f9a463
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2018-05-15 23:08:20 +0200

    Added comments to the Lexer header file.

--- sources/Node_Lexer.h
+++ sources/Node_Lexer.h
@@ -3,39 +3,68 @@
 #include "Node.h"       // base class
 #include <string>
 #include <vector>
-#include <utility>      // for std::pair
 
 namespace Nodable{
-	/* This enum identifies each kind of Token the lexer can handle */
-	enum TokenType_
-	{
-		TokenType_String   = 0,
-		TokenType_Number   = 1,
-		TokenType_Symbol   = 2,
-		TokenType_Operator = 3,
-		TokenType_Unknown  = 4
-	};
 
-	typedef struct
-	{
-		TokenType_  type      = TokenType_Unknown; // the type of the token
-		std::string word      = "";                // the word as a string
-		size_t      charIndex = 0;                 // the index of the first character of the token in the evaluated expression.
-	}Token;
+	/*
 
+		This class is build to generate a graph from an expression string.
+		You just have to instantiate it and set its member "expression" and call update();
+
+		auto ctx   = new Container("MyGlobalContainer");
+		auto lexer = ctx->createLexer();
+		lexer->setMember("expression", "10+50*40/0.001");
+		lexer->update();		
+		// here ctx should contain the generated graph.
+
+	*/
 	class Node_Lexer : public Node
 	{
+	private:
+
+		/* This enum identifies each kind of Token the lexer can handle */
+		enum TokenType_
+		{
+			TokenType_String   = 0,
+			TokenType_Number   = 1,
+			TokenType_Symbol   = 2,
+			TokenType_Operator = 3,
+			TokenType_Unknown  = 4
+		};
+
+		typedef struct
+		{
+			TokenType_  type      = TokenType_Unknown; // the type of the token
+			std::string word      = "";                // the word as a string
+			size_t      charIndex = 0;                 // the index of the first character of the token in the evaluated expression.
+		}Token;
+
 	public:
 		Node_Lexer();
 		virtual ~Node_Lexer();
+
+		/* Override from Node class */
 		bool           eval			       ()override;
 	private:
-		void           buildGraphRec       (size_t _tokenIndex, Node_Variable* _finalRes, Node_Variable* _prevRes = nullptr);
-		void           tokenize			   ();
-		bool           isSyntaxValid	   ();
+		/* Build the graph using existing tokens. tokenize() should be called first. */
 		Node_Variable* buildGraph          ();
+
+		/* Build the graph resursively starting at the token _tokenIndex.
+		The result of a single call will be stored into _finalRes and the result of the previous call is _prevRes */
+		void           buildGraphRec       (size_t _tokenIndex, Node_Variable* _finalRes, Node_Variable* _prevRes = nullptr);
+
+		/* Cut the member "expression" into tokens to identifies its type (cf. TokenType_ enum) */
+		void           tokenize			   ();
+
+		/* Check if the existing tokens match with the syntax of the language. tokenize() should be called first */
+		bool           isSyntaxValid	   ();
+
+		/* Convert a given token to a Node. For now it only handle Numbers, Strings and Symbols. Thats why we return a Node_Variable */
 		Node_Variable* convertTokenToNode  (Token token);
+		
+		/* Creates a new token given a _type, _string and _chanIndex and add it to the tokens.*/
 		void           addToken			   (TokenType_ _type, std::string _string, size_t _charIndex);
+
 		std::vector<Token> tokens;
 	};
 }
