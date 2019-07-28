342df5e5ed1e802d73e1c68cadbd6d63aae4b2e4
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2019-07-28 18:49:56 +0200

    spaces/tabulations refactor

--- sources/File.h
+++ sources/File.h
@@ -14,17 +14,18 @@ namespace Nodable
 	public:
 		File(const char* _path, const char* _content, const char* _name):content(_content),path(_path),name(_name), textEditorCursorPosition(0,0){}
 
-		std::string        getContent()const { return content; }
-		std::string        getName()const { return name; }	
+		std::string                      getContent()const { return content; }
+		std::string                      getName()const { return name; }	
 		const TextEditor::Coordinates&   getCursorPosition()const { return textEditorCursorPosition; }
 		void                             setCursorPosition(const TextEditor::Coordinates& _textEditorCursorPosition) { textEditorCursorPosition = _textEditorCursorPosition;  }
+		
+		static File*                     CreateFileWithPath                    (const char* _filePath);
+		static std::string               BrowseForFileAndReturnItsAbsolutePath (SDL_Window* currentWindow);
 
-		static File*       CreateFileWithPath                    (const char* _filePath);
-		static std::string BrowseForFileAndReturnItsAbsolutePath (SDL_Window* currentWindow);
 	private:
-		std::string content;
-		std::string path;
-		std::string name;
+		std::string               content;
+		std::string               path;
+		std::string               name;
 		TextEditor::Coordinates   textEditorCursorPosition;
 	};
 }
