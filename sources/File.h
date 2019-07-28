385c92ac21a170c83e9d8db22ff766b05da9ed89
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2019-07-28 18:49:20 +0200

    File now stores a TextEditor cursor coordinates

--- sources/File.h
+++ sources/File.h
@@ -5,16 +5,19 @@
 #include "Log.h"
 #include <string>
 #include <algorithm>
+#include "ImGuiColorTextEdit/TextEditor.h" // for coordinates
 
 namespace Nodable
 {
 	class File
 	{
 	public:
-		File(const char* _path, const char* _content, const char* _name):content(_content),path(_path),name(_name){}
+		File(const char* _path, const char* _content, const char* _name):content(_content),path(_path),name(_name), textEditorCursorPosition(0,0){}
 
 		std::string        getContent()const { return content; }
 		std::string        getName()const { return name; }	
+		const TextEditor::Coordinates&   getCursorPosition()const { return textEditorCursorPosition; }
+		void                             setCursorPosition(const TextEditor::Coordinates& _textEditorCursorPosition) { textEditorCursorPosition = _textEditorCursorPosition;  }
 
 		static File*       CreateFileWithPath                    (const char* _filePath);
 		static std::string BrowseForFileAndReturnItsAbsolutePath (SDL_Window* currentWindow);
@@ -22,5 +25,6 @@ namespace Nodable
 		std::string content;
 		std::string path;
 		std::string name;
+		TextEditor::Coordinates   textEditorCursorPosition;
 	};
 }
