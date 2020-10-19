c058867b1e1235aa9a1f236111919af2470235f2
Bérenger Dalle-Cort <berenger@dalle-cort.fr>
2020-10-19 21:12:23 +0200

    feat(OpenURL): moved from Application to System, use detached thread.

---
+++ sources/Core/System.cpp
@@ -0,0 +1,28 @@
+#include "System.h"
+#include "Log.h"
+#include <thread>
+
+void Nodable::System::OpenURL(std::string _URL)
+{
+    auto openURLambda = [](std::string _URL)
+    {
+#ifdef WIN32
+        std::string command("start");
+#else
+        std::string command("x-www-browser");
+#endif
+
+        std::string op = command + " " + _URL;
+        auto result = system(op.c_str());
+
+        if (result != 0)
+        {
+            LOG_ERROR( 0u, "Unable to open %s. Because the command %s is not available on your system.",
+                       _URL.c_str(), command.c_str());
+        }
+
+        return result;
+    };
+
+    std::thread( openURLambda, _URL).detach();
+}
