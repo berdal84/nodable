3c116b8c9a47033bb022b78563dfdb67adb42cea
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2020-11-16 23:58:42 +0100

    refactor(Application): split decl/impl and add some doc

--- sources/Core/Application.h
+++ sources/Core/Application.h
@@ -1,66 +1,138 @@
 #pragma once
 
-#include "Nodable.h" /* Forward declarations and defines */
-#include "Node.h"
-#include "History.h"
-#include "File.h"
+// std
 #include <string>
-#include <memory>    /* For unique_ptr */
+#include <memory>
 #include <mirror.h>
-#include <string>
 #include <filesystem>
 #include <future>
 
+// Nodable
+#include "Nodable.h" /* Forward declarations and defines */
+#include <Node/Node.h>
+#include <Component/History.h>
+#include <Core/File.h>
+
 namespace Nodable
 {
-
+    /**
+     * This class is the master class of Nodable.
+     * Using it you can launch a Nodable Application with ease.
+     *
+     * As you can see Application extends Node, because everything (or almost) is a Node in Nodable.
+     */
 	class Application : public Node
 	{
-
 	public:
-		Application(const char* /*_applicationName*/);
-		~Application();
 
-		/* Initialize the application.
-		Should be called once before the first update() and draw()*/
-		bool             init();
+	    /**
+	     * Construct a new Application given an application name argument.
+	     */
+		explicit Application(const char*);
 
-		/* Release resources */
-		void             shutdown();
+		~Application() override;
 
-		/* Update the state of the application.
-		Call this once per frame */
-		UpdateResult     update();
+		/**
+		 * Initialize the application.
+		 * Should be called once before the first update() and draw()
+         * @return true if succeed
+         */
+		bool init();
 
-		/* Force application to stops. Always delayed for 1 frame. */
-		void             stopExecution();
+		/**
+		 * Shutdown the application
+		 */
+		void shutdown();
 
-		bool             openFile(std::filesystem::path /* _filePath */);
+		/**
+		 * Update the state of the application.
+		 * this must be called once per frame
+		 * @return
+		 */
+		UpdateResult update() override;
 
-		/* get the asset path with a fileName (adds assetsFolder)*/
+		/**
+		 * Force application to stops.
+		 * The application will effectively stops after 1 frame.
+		 */
+		void stopExecution();
+
+		/**
+		 * Open a new file with a path
+		 * @param _filePath
+		 * @return true if succeed
+		 */
+		bool openFile(std::filesystem::path _filePath);
+
+		/**
+		 * Returns the full path of an asset from a filename.
+		 * @param _fileName
+		 * @return full path
+		 */
 		std::filesystem::path getAssetPath(const char* _fileName)const;
 
-		void             saveCurrentFile()const;
-		void             closeCurrentFile();
-		File*			 getCurrentFile()const;
-		size_t           getFileCount                          ()const              { return loadedFiles.size(); }
-		File*            getFileAtIndex                        (size_t _index)const { return loadedFiles[_index]; }
-		size_t           getCurrentFileIndex                   ()const              { return currentFileIndex; }
-		void             setCurrentFileWithIndex               (size_t _index);
-		
+		/**
+		 * Save the current file.
+		 * Does nothing if no file is open.
+		 */
+		void saveCurrentFile()const;
 
-		static void      SaveNode(Node* _entity);
+		/**
+		 * Close current file.
+		 * Does nothing if not file is open.
+		 */
+		void closeCurrentFile();
+
+		/**
+		 * Get the current file.
+		 * @return a pointer to the File, can be nullptr is no file is open.
+		 */
+		[[nodiscard]] File* getCurrentFile()const;
+
+		/**
+		 * Get the opened file count.
+		 * @return
+		 */
+		[[nodiscard]] size_t getFileCount()const;
+
+        /**
+         * Get the opened file by index.
+         * @param _index must be lower to the getFileCount().
+         * @return a pointer to the file, can't be nullptr.
+         */
+		[[nodiscard]] File*  getFileAtIndex(size_t _index)const;
+
+		/**
+		 * Get the index of the current file.
+		 * @return
+		 */
+		[[nodiscard]] size_t getCurrentFileIndex()const;
+
+		/**
+		 * Set the current file with an index.
+		 * @param _index must be lower that getFileCount().
+		 */
+		void setCurrentFileWithIndex(size_t _index);
+
+		/**
+		 * @deprecated
+		 *
+		 * Save a node to a file (in JSON)
+		 * @param _node
+		 */
+		static void SaveNode(Node* _node);
 
 	private:
-		/** A variable that contains (into the member "value") the current expression string */
-		Variable*     currentExpressionStringVariable  = nullptr;	
-
 		/** When set to true, the application will close next frame */
-		bool          quit                             = false;
+		bool quit = false;
 
+		/** The list of loaded files. */
 		std::vector<File*> loadedFiles;
-		size_t             currentFileIndex; /* index that identify the current file in loadedFiles */
-	
+
+        /** An index that identify the current file in loadedFiles */
+		size_t currentFileIndex;
+
+		/** The asset base folder path */
 		const std::filesystem::path assetsFolderPath;
 
 		/** Reflection using mirror*/
@@ -68,5 +140,3 @@ namespace Nodable
 
 	};
 }
-
-
