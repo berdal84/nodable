43a7018df858ae6423289f5a0822fc62ca3f4763
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2021-01-13 00:25:39 +0100

    NodeView: fix wire labels for vertical layout

--- sources/Core/Nodable.cpp
+++ sources/Core/Nodable.cpp
@@ -1,10 +1,11 @@
 #include "Nodable.h"
 
-namespace Nodable{
+namespace Nodable
+{
     float bezierCurveOutRoundness = 0.15f;
     float bezierCurveInRoundness  = 0.15f;
-    float bezierThickness         = 5.0f;
-	float connectorRadius         = 7.0f;
-	float nodePadding             = 10.0f;
+    float bezierThickness         = 4.0f;
+	float connectorRadius         = 5.0f;
+	float nodePadding             = 6.0f;
     bool  displayArrows           = false; 
 }
