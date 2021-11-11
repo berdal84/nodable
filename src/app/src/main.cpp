04e88c64bde389d3bd549a336793f2f8b1a32478
berdal84 <contact@dalle-cort.fr>
2021-11-10 23:41:12 -0500

    refactor(main): add mirror MACROS for class definitions

--- src/app/src/main.cpp
+++ src/app/src/main.cpp
@@ -1,11 +1,49 @@
 
+#include <mirror/mirror.h>
 #include <string>
 
 #include <nodable/Application.h>
 #include <nodable/ApplicationView.h>
 #include <nodable/Config.h>
+#include <nodable/GraphNodeView.h>
+#include <nodable/FileView.h>
+#include <nodable/ComputeBase.h>
+#include <nodable/GraphNode.h>
+#include <nodable/VariableNode.h>
+#include <nodable/LiteralNode.h>
+#include <nodable/ScopedCodeBlockNode.h>
+#include <nodable/ComputeUnaryOperation.h>
+#include <nodable/ComputeBinaryOperation.h>
+#include <nodable/Settings.h>
 
 using namespace Nodable;
+
+MIRROR_INITIALIZER
+
+// Declare reflected classes:
+MIRROR_CLASS_DEFINITION(Settings);
+// View and derived
+MIRROR_CLASS_DEFINITION(View)
+MIRROR_CLASS_DEFINITION(ApplicationView)
+MIRROR_CLASS_DEFINITION(FileView)
+MIRROR_CLASS_DEFINITION(Component)
+MIRROR_CLASS_DEFINITION(ComputeBase)
+MIRROR_CLASS_DEFINITION(ComputeFunction)
+MIRROR_CLASS_DEFINITION(ComputeUnaryOperation)
+MIRROR_CLASS_DEFINITION(ComputeBinaryOperation)
+MIRROR_CLASS_DEFINITION(NodeView) // inherits View and Component
+MIRROR_CLASS_DEFINITION(GraphNodeView) // inherits NodeView
+// Node and derived
+MIRROR_CLASS_DEFINITION(Node)
+MIRROR_CLASS_DEFINITION(GraphNode)
+MIRROR_CLASS_DEFINITION(VariableNode)
+MIRROR_CLASS_DEFINITION(LiteralNode)
+MIRROR_CLASS_DEFINITION(InstructionNode)
+MIRROR_CLASS_DEFINITION(AbstractCodeBlockNode)
+MIRROR_CLASS_DEFINITION(CodeBlockNode)
+MIRROR_CLASS_DEFINITION(ScopedCodeBlockNode)
+MIRROR_CLASS_DEFINITION(ConditionalStructNode)
+
 
 int main(int argc, char* argv[])
 {
@@ -35,4 +73,3 @@ int main(int argc, char* argv[])
     LOG_FLUSH();
 	return 0;
 }
-
