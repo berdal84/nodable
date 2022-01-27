fe092c32daed329a35551cdc310bf1bb7c3daf2e
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-01-27 13:38:55 -0500

    feat(Runner): "compiler"

--- src/core/include/nodable/Runner.h
+++ src/core/include/nodable/Runner.h
@@ -7,6 +7,7 @@ namespace Nodable
 {
     // forward declarations
     class ScopedCodeBlockNode;
+    struct SimpleInstrList;
 
     /**
      * Class to execute a Nodable program.
@@ -25,18 +26,141 @@ namespace Nodable
         [[nodiscard]] inline bool             is_program_stopped() const{ return !m_is_debugging && !m_is_program_running; }
                              bool             step_over();
         [[nodiscard]] inline const Node*      get_current_node() const {return m_current_node; }
-                      inline InstructionNode* get_last_evaluated_instr() { return m_last_evaluated_instr; }
+                      inline Member*          get_last_eval() { return m_last_eval; }
 
     private:
+        SimpleInstrList*      compile_program(const ScopedCodeBlockNode* _program);
+        void                  compile_node_and_append_to_program(const Node* _node);
         bool                  is_program_valid(const ScopedCodeBlockNode* _program);
         bool                  _stepOver();
         GraphTraversal        m_traversal;
         ScopedCodeBlockNode*  m_program;
+        SimpleInstrList*      m_compiled_program;
         Node*                 m_current_node;
-        InstructionNode*      m_last_evaluated_instr;
+        Member*               m_last_eval;
         bool                  m_is_program_running;
         bool                  m_is_debugging;
+        bool                  m_registers[1]; // TODO: organise a set of flags in a char.
     };
+
+
+    enum SIType
+    {
+        Type_UNDEF,
+        Type_EVAL,
+        Type_STORE,
+        Type_JUMP,
+        Type_JUMP_IF_FALSE,
+        Type_EXIT,
+    };
+
+    class SimpleInstr
+    {
+    public:
+
+        SimpleInstr(SIType _type, long _line):m_type(_type), m_line(_line){}
+
+        SimpleInstr(const SimpleInstr& _other)
+        :
+        m_type(_other.m_type),
+        m_line(_other.m_line),
+        m_data(_other.m_data),
+        m_comment(_other.m_comment)
+        {
+        }
+
+        std::string to_string()
+        {
+            std::string result;
+
+            result.append( std::to_string(m_line) );
+            result.append( ": " );
+
+            switch ( m_type )
+            {
+                case Type_EVAL:
+                {
+                    Member* member = mpark::get<Member*>( m_data );
+                    result.append("EVAL");
+                    result.append( " " );
+                    result.append( member->getOwner()->get_class()->get_name() );
+                    result.append( "->" );
+                    result.append( member->getName() );
+                    break;
+                }
+
+                case Type_STORE:
+                {
+                    result.append("STOR");
+                    break;
+                }
+
+                case Type_JUMP_IF_FALSE:
+                {
+                    result.append("CJMP");
+                    result.append( " " );
+                    result.append( std::to_string( mpark::get<long>( m_data ) ) );
+                    break;
+                }
+
+                case Type_JUMP:
+                {
+                    result.append("JUMP");
+                    result.append( " " );
+                    result.append( std::to_string( mpark::get<long>( m_data ) ) );
+                    break;
+                }
+
+                case Type_UNDEF:
+                {
+                    result.append("UNDEF");
+                    break;
+                }
+
+                case Type_EXIT:
+                {
+                    result.append("EXIT");
+                    break;
+                }
+
+            }
+            return result;
+        }
+
+        SIType m_type;
+        long   m_line;
+        mpark::variant<void* , Node*, Member*, long> m_data;
+        std::string m_comment;
+    };
+
+    /**
+     * Class to store a simple instruction list and navigate forward through it
+     */
+     class SimpleInstrList
+     {
+     public:
+         SimpleInstrList(){}
+         ~SimpleInstrList()
+         {
+             for( auto each : m_instructions )
+                 delete each;
+             m_instructions.clear();
+         }
+         void         advance(long _amount = 1) { m_cursor_position += _amount; }
+         SimpleInstr* push_instr(SIType _type)
+         {
+             SimpleInstr* instr = new SimpleInstr(_type, m_instructions.size());
+             m_instructions.emplace_back(instr);
+             return instr;
+         };
+         SimpleInstr* get_curr(){ return m_cursor_position < m_instructions.size() ? m_instructions[m_cursor_position] : nullptr; };
+         void         reset_cursor(){ m_cursor_position = 0; };
+         long         get_next_line_nb(){ return m_instructions.size(); }
+         bool         is_over() { assert(get_curr()); return get_curr()->m_type == Type_EXIT; }
+     private:
+         size_t m_cursor_position = 0;
+         std::vector<SimpleInstr*> m_instructions;
+     };
 }
 
 
--- src/core/src/Runner.cpp
+++ src/core/src/Runner.cpp
@@ -5,6 +5,7 @@
 #include <nodable/VariableNode.h>
 #include <nodable/Log.h>
 #include <nodable/AbstractConditionalStruct.h>
+#include <nodable/ForLoopNode.h>
 
 using namespace Nodable;
 
@@ -14,21 +15,136 @@ Runner::Runner()
         m_is_debugging(false),
         m_is_program_running(false),
         m_current_node(nullptr),
-        m_last_evaluated_instr(nullptr)
+        m_last_eval(nullptr),
+        m_compiled_program(nullptr)
 {
 
+}
+
+void Runner::compile_node_and_append_to_program(const Node* _node)
+{
+    if ( _node )
+    {
+        if ( _node->get_class()->is<AbstractConditionalStruct>() )
+        {
+            auto cond = _node->as<AbstractConditionalStruct>();
+            // TODO: insert an EvalAndStoreResult on node condition,
+            // TODO: insert a jump depending on last result to go to "true" scope,
+            // TODO: if "false" branch exists, insert a jump to go,
+
+            // for_loop init instruction
+            if ( auto for_loop = _node->as<ForLoopNode>() )
+            {
+                auto init_instr = m_compiled_program->push_instr(Type_EVAL  );
+                init_instr->m_data.emplace<Member*>( for_loop->get_init_expr() );
+                init_instr->m_comment = "init-loop";
+            }
+
+            SimpleInstr* cond_instr = m_compiled_program->push_instr(Type_EVAL);
+            cond_instr->m_data.emplace<Member*>( cond->get_condition() );
+            cond_instr->m_comment = "condition";
+
+            SimpleInstr* store_instr = m_compiled_program->push_instr(Type_STORE);
+            store_instr->m_comment = "store cond result";
+
+            SimpleInstr* skip_true_branch = m_compiled_program->push_instr(Type_JUMP_IF_FALSE);
+            skip_true_branch->m_comment = "jump if register is false";
+
+            SimpleInstr* skip_false_branch = nullptr;
+
+            if ( auto true_branch = cond->get_condition_true_branch() )
+            {
+                compile_node_and_append_to_program(true_branch);
+
+                if ( auto for_loop = _node->as<ForLoopNode>() )
+                {
+                    // insert end-loop instruction.
+                    auto end_loop_instr = m_compiled_program->push_instr(Type_EVAL);
+                    end_loop_instr->m_data = for_loop->get_iter_expr();
+
+                    // insert jump to condition instructions.
+                    auto loop_jump = m_compiled_program->push_instr(Type_JUMP);
+                    loop_jump->m_data = cond_instr->m_line - loop_jump->m_line;
+                    loop_jump->m_comment = "end-loop";
+
+                }
+                else if (cond->get_condition_false_branch())
+                {
+                    skip_false_branch = m_compiled_program->push_instr(Type_JUMP);
+                    skip_false_branch->m_comment = "jump false branch";
+                }
+            }
+
+            skip_true_branch->m_data = m_compiled_program->get_next_line_nb() - skip_true_branch->m_line;
+
+            if ( auto false_branch = cond->get_condition_false_branch() )
+            {
+                compile_node_and_append_to_program(false_branch);
+                skip_false_branch->m_data = m_compiled_program->get_next_line_nb() - skip_false_branch->m_line;
+            }
+        }
+        else if ( _node->get_class()->is<AbstractCodeBlock>() )
+        {
+            for( auto each : _node->get_children() )
+            {
+                compile_node_and_append_to_program(each);
+            }
+        }
+        else
+        {
+            SimpleInstr* instr = m_compiled_program->push_instr(Type_EVAL);
+            instr->m_data = _node->getProps()->get("value");
+        }
+    }
+
+}
+SimpleInstrList* Runner::compile_program(const ScopedCodeBlockNode* _program)
+{
+    /*
+     * Here we take the program's base scope node (a tree) and we flatten it to an
+     * instruction list. We add some jump instruction in order to skip portions of code.
+     * This works "a little bit" like a compiler, at least for the "tree to list" point of view.
+     */
+    delete m_compiled_program;
+    m_compiled_program = new SimpleInstrList();
+    compile_node_and_append_to_program(_program);
+    m_compiled_program->push_instr(Type_EXIT);
+    return m_compiled_program;
 }
 
 bool Runner::load_program(ScopedCodeBlockNode* _program)
 {
     if ( is_program_valid(_program) )
     {
+        // unload current program
         if (m_program)
         {
             unload_program();
         }
-        m_program = _program;
-        return true;
+
+        if ( compile_program(_program) )
+        {
+            m_program = _program;
+            LOG_MESSAGE("Runner", "Program's tree compiled.\n");
+            LOG_VERBOSE("Runner", "Find bellow the compilation result:\n");
+            LOG_VERBOSE("Runner", "---- Program begin -----\n");
+            SimpleInstr* curr = m_compiled_program->get_curr();
+            while( curr )
+            {
+                LOG_VERBOSE("Runner", "%s  // %s\n", curr->to_string().c_str(), curr->m_comment.c_str() );
+                m_compiled_program->advance(1);
+                curr = m_compiled_program->get_curr();
+            }
+            m_compiled_program->reset_cursor();
+            LOG_VERBOSE("Runner", "---- Program end -----\n");
+            return true;
+        }
+        else
+        {
+            LOG_ERROR("Runner", "Unable to compile program's tree.\n");
+            return false;
+        }
+
     }
     return false;
 }
@@ -61,17 +177,6 @@ void Runner::run_program()
     LOG_VERBOSE("Runner", "Running...\n")
     m_is_program_running = true;
 
-    /*
-     * Strategy:
-     *
-     * - create execution context
-     * - get the first instruction
-     * - eval, get next, eval, etc..
-     * - when next is nullptr: STOP
-     */
-
-    // temp poor update
-    m_current_node = m_traversal.getNextInstrToEval(m_program);
     while(!is_program_over())
     {
         _stepOver();
@@ -94,34 +199,87 @@ void Runner::unload_program() {
 
 bool Runner::_stepOver()
 {
-    m_traversal.traverse(m_current_node, TraversalFlag_FollowInputs | TraversalFlag_FollowNotDirty | TraversalFlag_AvoidCycles );
-    size_t total(m_traversal.getStats().m_traversed.size());
-    size_t idx = 1;
-    for(auto* eachNodeToEval : m_traversal.getStats().m_traversed)
+    bool success;
+    SimpleInstr* curr_instr = m_compiled_program->get_curr();
+
+    LOG_VERBOSE("Runner", "processing line %i.\n", (int)curr_instr->m_line );
+
+    switch ( curr_instr->m_type )
     {
-        if( eachNodeToEval->get_class()->is<AbstractConditionalStruct>() )
+        case Type_UNDEF:
         {
-            // we first need to evaluates its conditions
-            auto* conditional = eachNodeToEval->as<AbstractConditionalStruct>();
-            GraphTraversal temp_traversal;
-            temp_traversal.traverse(conditional->get_condition()->getOwner(), TraversalFlag_FollowInputs | TraversalFlag_FollowNotDirty | TraversalFlag_AvoidCycles );
-            for(auto* node : m_traversal.getStats().m_traversed) {
-                node->eval();
-                node->setDirty(false);
+            LOG_ERROR("Runner", "Instruction %i is undefined.\n", (int)curr_instr->m_line);
+            m_compiled_program->advance();
+            success = false;
+            break;
+        }
+
+        case Type_STORE:
+        {
+            NODABLE_ASSERT(m_last_eval);
+            m_registers[0] = m_last_eval->convert_to<bool>();
+            m_compiled_program->advance();
+            success = true;
+            break;
+        }
+
+        case Type_EVAL:
+        {
+            // TODO: traverse graph in advance during compilation step.
+            Member* member = mpark::get<Member*>(curr_instr->m_data);
+            m_current_node = member->getOwner();
+
+            if ( Member* input = member->getInput() )
+            {
+                m_traversal.traverse(input->getOwner(), TraversalFlag_FollowInputs | TraversalFlag_FollowNotDirty |
+                                                         TraversalFlag_AvoidCycles);
+                size_t total(m_traversal.getStats().m_traversed.size());
+                size_t idx = 1;
+                for (auto *eachNodeToEval : m_traversal.getStats().m_traversed) {
+                    eachNodeToEval->eval();
+                    eachNodeToEval->setDirty(false);
+                    LOG_VERBOSE("Runner", "Eval (%i/%i): \"%s\" (class %s) \n", idx, (int) total,
+                                eachNodeToEval->getLabel(), eachNodeToEval->get_class()->get_name())
+                    idx++;
+                }
             }
+
+            m_last_eval = member;
+            m_compiled_program->advance();
+            success = true;
+            break;
         }
 
-        eachNodeToEval->eval();
-        eachNodeToEval->setDirty(false);
-        if ( eachNodeToEval->get_class() == InstructionNode::Get_class())
+        case Type_JUMP:
         {
-            m_last_evaluated_instr = eachNodeToEval->as<InstructionNode>();
+            m_compiled_program->advance( mpark::get<long>(curr_instr->m_data) );
+            success = true;
+            break;
         }
 
-        LOG_VERBOSE("Runner", "Eval (%i/%i): \"%s\" (class %s) \n", idx, (int)total, eachNodeToEval->getLabel(), eachNodeToEval->get_class()->get_name())
-        idx++;
+        case Type_JUMP_IF_FALSE:
+        {
+            if ( m_registers[0] )
+            {
+                m_compiled_program->advance();
+            }
+            else
+            {
+                m_compiled_program->advance( mpark::get<long>(curr_instr->m_data) );
+            }
+            success = true;
+            break;
+        }
+
+        case Type_EXIT:
+            success = true;
+            break;
+
+        default:
+            success = false;
     }
-    m_current_node = m_traversal.getNextInstrToEval(m_current_node);
+
+    return success;
 }
 
 bool Runner::step_over()
@@ -131,13 +289,14 @@ bool Runner::step_over()
     if( !continue_execution )
     {
         stop_program();
+        m_current_node = nullptr;
     }
     return continue_execution;
 }
 
 bool Runner::is_program_over()
 {
-    return m_current_node == nullptr;
+    return m_compiled_program->is_over();
 }
 
 void Runner::debug_program()
@@ -145,5 +304,6 @@ void Runner::debug_program()
     NODABLE_ASSERT(this->m_program != nullptr);
     m_is_debugging = true;
     m_is_program_running = true;
-    m_current_node = m_traversal.getNextInstrToEval(m_program);
+    m_compiled_program->reset_cursor();
+    m_current_node = m_program;
 }
