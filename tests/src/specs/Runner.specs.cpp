e08490de19af6a4c5a11410aefb15a1a3d4f4ea1
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-01-27 14:49:23 -0500

    test(Runner): mix if/else with loop

--- tests/src/specs/Runner.specs.cpp
+++ tests/src/specs/Runner.specs.cpp
@@ -48,3 +48,22 @@ TEST(Runner, Loop_1 )
 
     EXPECT_EQ( ParseAndEvalExpression<std::string>(program), "0123456789" );
 }
+
+TEST(Runner, Loop_2 )
+{
+    std::string program =
+            "double n;"
+            "double p;"
+            "string res = \"\";"
+            "for(n=0;n<10;n=n+1){"
+            "   p = pow(n,2);"
+            "   if( p != n ){         /* skip powers equals to n */"
+            "      res = res + p;     /* concat powers */"
+            "   }else{"
+            "      res = res + \"_\"; /* concat \"_\" */"
+            "   }"
+            "}"
+            "res;";
+
+    EXPECT_EQ( ParseAndEvalExpression<std::string>(program), "__49162536496481" );
+}
