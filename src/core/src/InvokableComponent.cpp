b97dd855177750b8b243ed81919aae541a8d07d2
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-03-05 17:49:45 -0500

    fix(InvokableComponent): avoid to invoke when members are from an undeclared var

--- src/core/src/InvokableComponent.cpp
+++ src/core/src/InvokableComponent.cpp
@@ -1,6 +1,7 @@
 #include <nodable/InvokableComponent.h>
 #include <nodable/Log.h>
 #include <nodable/Language.h>
+#include <nodable/VariableNode.h>
 
 using namespace Nodable;
 
@@ -21,17 +22,33 @@ bool InvokableComponent::update()
 {
     bool success = true;
 
-    try
+    /** check if a member is an undeclared variable */
+    auto found_member_not_declared = std::find_if(m_args.begin(), m_args.end(), [](Member* _member)
     {
-        m_invokable->invoke(m_result, m_args);
-    }
-    catch (std::runtime_error& err)
+        auto var = _member->get_owner()->as<VariableNode>();
+        return var && !var->is_declared();
+    });
+
+    /** in case we found one, we do not invoke the invokable function */
+    if (found_member_not_declared != m_args.end() )
     {
-        LOG_ERROR("InvokableComponent", "Exception thrown during update %s:\n"
-                                        " - while updating Node %s\n"
-                                        " - reason: %s\n", get_owner()->get_label(), err.what() )
         success = false;
     }
+    else
+    {
+        try
+        {
+            m_invokable->invoke(m_result, m_args);
+        }
+        catch (std::exception& err)
+        {
+            LOG_ERROR("InvokableComponent", "Exception thrown during update %s:\n"
+                                            " - while updating Node %s\n"
+                                            " - reason: %s\n", get_owner()->get_label(), err.what() )
+            success = false;
+        }
+    }
+
 
 	return success;
 }
