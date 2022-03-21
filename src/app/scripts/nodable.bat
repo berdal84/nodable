7091de49eea1422d4f2ff9803f52e5711c598302
Berenger Dalle-Cort <berenger@dalle-cort.fr>
2022-03-21 10:29:17 -0400

    chore(cmake): add nodable.bat

---
+++ src/app/scripts/nodable.bat
@@ -0,0 +1,7 @@
+@echo off
+SET NODABLE_DIR=%~dp0
+SET LOG_PREFIX=[MSG-Launcher]
+echo %LOG_PREFIX% nodable base dir is: %NODABLE_DIR:~0,-1%
+echo %LOG_PREFIX% Running nodable.exe ...
+start /wait /d %NODABLE_DIR% %NODABLE_DIR%bin\nodable.exe
+echo %LOG_PREFIX% nodable.exe stopped.
