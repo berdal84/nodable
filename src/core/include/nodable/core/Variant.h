f676b8aec8d0784b75b2e6d319ca3c2a921ca9f6
berdal84 <berenger@dalle-cort.fr>
2022-03-24 17:11:45 -0400

    fix(Variant): remove mpark::variant, use union, strict types WIP 20

--- src/core/include/nodable/core/Variant.h
+++ src/core/include/nodable/core/Variant.h
@@ -91,13 +91,13 @@ namespace Nodable
         template<typename T> operator const T*()const  { NODABLE_ASSERT(m_is_defined) return reinterpret_cast<const T*>(m_data.m_void_ptr); }
         template<typename T> operator T*()             { NODABLE_ASSERT(m_is_defined) return reinterpret_cast<T*>(m_data.m_void_ptr); }
 
-		operator double*()        { NODABLE_ASSERT(m_is_defined) return &m_data.m_double; }
-        operator bool*()          { NODABLE_ASSERT(m_is_defined) return &m_data.m_bool; }
-        operator std::string* ()  { NODABLE_ASSERT(m_is_defined) return m_data.m_std_string_ptr; }
-        operator void* ()         { NODABLE_ASSERT(m_is_defined) return m_data.m_void_ptr; }
-        operator double&()        { NODABLE_ASSERT(m_is_defined) return m_data.m_double; }
-        operator bool&()          { NODABLE_ASSERT(m_is_defined) return m_data.m_bool; }
-        operator std::string& ()  { NODABLE_ASSERT(m_is_defined) return *m_data.m_std_string_ptr; }
+		operator double*()        { NODABLE_ASSERT(m_is_initialized) return &m_data.m_double; }
+        operator bool*()          { NODABLE_ASSERT(m_is_initialized) return &m_data.m_bool; }
+        operator std::string* ()  { NODABLE_ASSERT(m_is_initialized) return m_data.m_std_string_ptr; }
+        operator void* ()         { NODABLE_ASSERT(m_is_initialized) return m_data.m_void_ptr; }
+        operator double&()        { NODABLE_ASSERT(m_is_initialized) return m_data.m_double; }
+        operator bool&()          { NODABLE_ASSERT(m_is_initialized) return m_data.m_bool; }
+        operator std::string& ()  { NODABLE_ASSERT(m_is_initialized && m_is_defined) return *m_data.m_std_string_ptr; }
 
         // by value
         operator int()const;
