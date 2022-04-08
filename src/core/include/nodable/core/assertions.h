5f6b1eb117d1156316ca9f931f8b442acf3c7b7e
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-04-08 11:21:31 -0400

    feat(assertions): add an exception based macro

--- src/core/include/nodable/core/assertions.h
+++ src/core/include/nodable/core/assertions.h
@@ -1,7 +1,27 @@
 #pragma once
 
-#include <cassert>
-#include <nodable/core/Log.h>
+#include <nodable/core/Log.h> // to flush before to assert/throw
 
-// Ensure to flush log before to assert
-#define NODABLE_ASSERT(expression) LOG_FLUSH(); assert((expression));
+// Assertion
+#define _NODABLE_ASSERT_NOEXCEPT(expression) \
+    LOG_FLUSH(); \
+    assert((expression));
+
+// Assertion throwing an exception
+#define _NODABLE_ASSERT(expression, message_if_fails )\
+    LOG_FLUSH(); \
+    if(!(expression)) throw std::runtime_error(message_if_fails);
+
+// Assert shortcut
+#if NOEXCEPT
+#   include <cassert>
+#   define NODABLE_ASSERT(expression)             _NODABLE_ASSERT_NOEXCEPT( expression )
+#   NODABLE_ASSERT_EX(expression, message)        _NODABLE_ASSERT_NOEXCEPT( expression )
+#else
+#   include <stdexcept>
+#   define NODABLE_ASSERT(expression)             _NODABLE_ASSERT( (expression), "assertion failed" )
+#   define NODABLE_ASSERT_EX(expression, message) _NODABLE_ASSERT( (expression), message )
+#endif
+
+#undef _NODABLE_ASSERT_NOEXCEPT
+#undef _NODABLE_ASSERT_EXCEPT
