4fb0fa75cc62b2a1bf5f6c6f896b02237a9a91e7
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-04-19 11:52:22 -0400

    refactor(NodableLanguage); move signature_builder to separate file

--- src/core/include/nodable/core/ILanguage.h
+++ src/core/include/nodable/core/ILanguage.h
@@ -65,4 +65,64 @@ namespace Nodable {
         virtual std::string                     sanitize_function_id(const std::string& _id)const = 0;
         virtual std::string                     sanitize_operator_id(const std::string& _id)const = 0;
     };
+
+    /**
+     * Builder to create function/operator signatures for a given language
+     * @tparam T is the function's return type
+     * @tparam Args is the function's argument(s) type
+     *
+     * usage: Signature* sig = signature-builder<double(double,double)>::signature()
+     *                                                                  .with_id("+")
+     *                                                                  .as_operator()
+     *                                                                  .with_language(lang_ptr).build();
+     */
+    template<typename T, typename ...Args>
+    struct signature_builder<T(Args...)>
+    {
+        using this_t = signature_builder<T(Args...)>;
+
+        std::string       m_id;
+        const ILanguage*  m_language;
+        bool              m_as_operator;
+
+        signature_builder()
+                : m_as_operator(false)
+                , m_language(nullptr){}
+
+        void with_id(const std::string& _id) { m_id = _id; }
+        void with_lang(const ILanguage* _language) { m_language = _language; }
+        void as_operator(bool _b = true) { m_as_operator = _b; }
+
+        Signature* construct()
+        {
+            NODABLE_ASSERT_EX( m_language   , "No language specified! use with_lang()" );
+            NODABLE_ASSERT_EX( !m_id.empty(), "No identifier specified! use with_id()" );
+
+            Signature* signature;
+            if( m_as_operator )
+            {
+                const Operator* op;
+                size_t argc = sizeof...(Args);
+                switch ( argc )
+                {
+                    case 1:  op = m_language->find_operator(m_id, Operator_t::Unary);  break;
+                    case 2:  op = m_language->find_operator(m_id, Operator_t::Binary); break;
+                    default: op = nullptr;
+                }
+                NODABLE_ASSERT_EX( op, "No operator found  in language for specified id!" );
+                signature = new Signature(m_language->sanitize_operator_id(op->identifier), op);
+            }
+            else
+            {
+                m_id = m_language->sanitize_function_id(m_id);
+                NODABLE_ASSERT_EX( !m_id.empty(), "Identifier after sanitization is empty!" );
+                signature = new Signature( m_id );
+            }
+
+            signature->set_return_type(type::get<T>());
+            signature->push_args<std::tuple<Args...>>();
+
+            return signature;
+        }
+    };
 }
