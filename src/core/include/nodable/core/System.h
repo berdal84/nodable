6d53a53590434ae3552c364587b71ef84b2d2433
Bérenger DALLE-CORT <berenger@42borgata.com>
2023-01-13 20:35:50 -0500

    refactor(System): documentation

--- src/core/include/nodable/core/System.h
+++ src/core/include/nodable/core/System.h
@@ -4,15 +4,21 @@
 namespace ndbl
 {
     /**
-     * Static library cross platform to deal with system
+     * @namespace Set of cross platform functions library to deal with system
      */
     namespace System
     {
+        /** Browse a given URL asynchronously */
         extern void        open_url_async(std::string /* url */);
+        /** Get the path to the executable directory */
         extern std::string get_executable_directory();
 
+        /**
+         * @namespace Set of cross platform functions library to deal with the console
+         */
         namespace console
         {
+            /** Clear the console */
             extern void clear();
         }
 
