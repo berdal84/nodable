7de94beb42deb1ab1c87976fc662cce123e5d042
Bérenger DALLE-CORT <berenger@42borgata.com>
2023-01-13 20:08:53 -0500

    refactor(types): documentation

--- src/core/include/nodable/core/types.h
+++ src/core/include/nodable/core/types.h
@@ -6,6 +6,10 @@
 
 namespace ndbl
 {
+    /**
+     * This file contains basic type declarations (and some forward declarations) we use everywhere in Nodable code.
+     */
+
     // forward declarations
 	class Wire;
 	class Member;
@@ -14,16 +18,19 @@ namespace ndbl
     class NodeView;
     class VariableNode;
 
+    // integers
     typedef long long i64_t;
     typedef long      i32_t;
     typedef int       i16_t;
     typedef short int i8_t;
 
+    // unsigned integers
     typedef unsigned long long u64_t;
     typedef unsigned long      u32_t;
     typedef unsigned int       u16_t;
     typedef unsigned short int u8_t;
 
+    // containers
     typedef std::map<std::string, Component*>       Components;
     typedef std::map<std::string, Member*>          MemberMap;
     typedef std::vector<Member*>                    MemberVec;
