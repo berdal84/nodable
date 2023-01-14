e2c4486f2e9f2057e6ec049e3b4e53e9191eeb53
berdal84 <berenger@42borgata.com>
2023-01-14 11:21:30 -0500

    refactor(ForLoopNode): doc, cleaning

--- src/core/include/nodable/core/ForLoopNode.h
+++ src/core/include/nodable/core/ForLoopNode.h
@@ -7,36 +7,41 @@
 
 namespace ndbl
 {
-
     // forward declarations
     class Scope;
     class InstructionNode;
 
     /**
-     * @brief Class to represent a conditional iterative structure: for( init_state, condition_expr, iterate_expr ) { ... }
+     * @class Represent a conditional and iterative structure "for"
+     * for( init_state, condition_expr, iterate_expr ) { ... }
      */
     class ForLoopNode
-            : public Node
-            , public IConditionalStruct {
+        : public Node
+        , public IConditionalStruct {
     public:
         ForLoopNode();
         ~ForLoopNode() = default;
 
         inline void            set_token_for(std::shared_ptr<Token> _token) { m_token_for = _token; }
         inline std::shared_ptr<const Token> get_token_for()const   { return m_token_for; }
-        Member*                get_init_expr()const { return m_props.get(k_forloop_initialization_member_name); }
-        Member*                get_iter_expr()const { return m_props.get(k_forloop_iteration_member_name); }
 
-        // override IConditionalStruct
-        Member* condition_member()const override { return m_props.get(k_condition_member_name);}
-        Scope*  get_condition_true_scope()const override;
-        Scope*  get_condition_false_scope()const override;
-        void set_iter_instr(InstructionNode*);
-        void set_init_instr(InstructionNode*);
-        void set_cond_expr(InstructionNode*) override;
-        InstructionNode* get_init_instr()const { return m_init_instr_node; }
-        InstructionNode*get_cond_expr()const override { return m_cond_instr_node; }
+        // TODO: create IIterativeStruct to reuse for a future "while" node.
+
+        Member*          get_init_expr()const { return m_props.get(k_forloop_initialization_member_name); }
+        Member*          get_iter_expr()const { return m_props.get(k_forloop_iteration_member_name); }
         InstructionNode* get_iter_instr()const { return m_iter_instr_node; }
+        InstructionNode* get_init_instr()const { return m_init_instr_node; }
+        void             set_iter_instr(InstructionNode*);
+        void             set_init_instr(InstructionNode*);
+
+        // implements IConditionalStruct (which is already documented)
+
+        Member*          condition_member()const override { return m_props.get(k_condition_member_name);}
+        Scope*           get_condition_true_scope()const override;
+        Scope*           get_condition_false_scope()const override;
+        void             set_cond_expr(InstructionNode*) override;
+        InstructionNode* get_cond_expr()const override { return m_cond_instr_node; }
+
     private:
         std::shared_ptr<Token> m_token_for;
         InstructionNode* m_init_instr_node;
