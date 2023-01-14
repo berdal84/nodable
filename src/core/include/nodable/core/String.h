80de21e1060e7904427baeb3b95119516b2e195b
Bérenger DALLE-CORT <berenger@42borgata.com>
2023-01-13 20:38:09 -0500

    refactor(String): documentation

--- src/core/include/nodable/core/String.h
+++ src/core/include/nodable/core/String.h
@@ -5,11 +5,17 @@
 
 namespace ndbl
 {
+    /**
+     * @class Static library to deal with string formatting
+     */
     class String
     {
     public:
+        /** Format a double to a string (without trailing zeros)*/
         static std::string fmt_double(double);
+        /** Format an qword as an hexadecimal string */
         static std::string fmt_hex(u64_t _addr);
+        /** Format an address as an hexadecimal string */
         static std::string fmt_ptr(const void* _addr);
     private:
         static void limit_trailing_zeros(std::string& str, int _trailing_max);
