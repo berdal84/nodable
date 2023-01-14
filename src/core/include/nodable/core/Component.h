bc45e74a909434132ae63e457535ff27a4b69ceb
berdal84 <berenger@42borgata.com>
2023-01-14 10:21:14 -0500

    refactor(Component): visibility, add documentation

--- src/core/include/nodable/core/Component.h
+++ src/core/include/nodable/core/Component.h
@@ -5,21 +5,34 @@
 namespace ndbl{
 
     class Node;
-    
+
+    /**
+     * @class Base abstract class for any Node Component
+     */
 	class Component
 	{
-	public:
+        friend Node;
+    public:
+        /** Called each frame */
+        virtual bool  update() = 0;
 
+        /** Get the node owning this component */
+        inline Node*  get_owner()const { return m_owner; }
+
+        /** Shorthand to cast<T>(this) */
+        template<class T> [[nodiscard]] inline       T* as()      { return cast<T>(this); }
+
+        /** Shorthand to cast<const T>(this) */
+        template<class T> [[nodiscard]] inline const T* as()const { return cast<const T>(this); }
+
+	protected:
 		Component() = default;
 		virtual ~Component() = default;
         Component (const Component&) = delete;
         Component& operator= (const Component&) = delete;
+        /** Set the node owning this component */
+		virtual void  set_owner(Node* node){ m_owner = node; }
 
-		virtual bool         update() = 0;
-		virtual inline void  set_owner(Node *_entity){ m_owner = _entity; }
-		inline Node*         get_owner()const { return m_owner; }
-		template<class T> [[nodiscard]] inline       T* as()      { return cast<T>(this); }
-        template<class T> [[nodiscard]] inline const T* as()const { return cast<const T>(this); }
 	private:
 		Node* m_owner = nullptr;
 		REFLECT_BASE_CLASS()
