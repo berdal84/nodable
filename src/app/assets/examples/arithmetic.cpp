476e675ad7df44d5d950c6ea80c712a8dd457a15
berdal84 <berenger@42borgata.com>
2023-01-16 16:37:43 -0500

    fix(app): examples

--- src/app/assets/examples/arithmetic.cpp
+++ src/app/assets/examples/arithmetic.cpp
@@ -1,15 +1,11 @@
 
 // Arithmetic expressions example
 
-// Tips:
-// - Use isolation mode to see the graph of the current line or selection.
-// - Use CTRL + I to toggle isolation mode on/off.
-
 // expression 1:
-1+1;
+int a = 1+1;
 
 // expression 2:
-15 * 2 + 5 / 2;
+int b = 15 * 2 + 5 / 2;
 
 // expression 3:
-(9*9 + 9) / 45;
+int c = (9*9 + 9) / 45;
