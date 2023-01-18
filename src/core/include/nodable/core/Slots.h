0496a4f73211dfa033093f5a27ec190253c2052f
berdal84 <berenger@42borgata.com>
2023-01-17 20:57:23 -0500

    refactor(Slots): comments

--- src/core/include/nodable/core/Slots.h
+++ src/core/include/nodable/core/Slots.h
@@ -12,70 +12,50 @@ namespace ndbl
     template<typename T>
     struct Slots
     {
-        Slots() = delete;
-        Slots(T _parent, size_t _max_count = std::numeric_limits<size_t>::max())
-            : m_parent(_parent)
-            , m_limit(_max_count) {}
+        Slots(size_t _max_count = std::numeric_limits<size_t>::max())
+            : m_limit(_max_count) {}
 
-        /** Get slots as a vector */
-        std::vector<T>&       content() { return m_slots; }
-        /** Get slots as a vector */
-        const std::vector<T>& content() const { return m_slots; }
-        /** Get an iterator at the beginning of the slots */
-        auto                  begin() const { return m_slots.begin(); }
-        /** Get an iterator at the end of the slots */
-        auto                  end() const { return m_slots.end(); }
-        /** Set a slot count limit */
-        void                  set_limit(size_t _count) { m_limit = _count; }
-        /** Get the slot count limit */
-        size_t                get_limit()const { return m_limit; }
-        /** Get the last slot (check if not empty() before) */
-        T                     back() { return m_slots.back(); }
-        /** Check if slots are empty */
-        bool                  empty() const { return m_slots.empty(); }
-        /** Get the slot count */
-        size_t                size() const { return m_slots.size(); }
-        /** Get the element at zero-based index */
-        T                     operator[](size_t _index) const { return m_slots[_index]; }
-        /** Check if accepts new elements (depends on limit) */
-        bool                  accepts() const { return m_slots.size() < m_limit; }
+        std::vector<T>&       content() { return m_slots; }       // Get slots content as a vector
+        const std::vector<T>& content() const { return m_slots; } // Get slots content as a vector
+        auto      begin() const { return m_slots.begin(); }       // Get an iterator at the beginning of the slots
+        auto      end() const { return m_slots.end(); }           // Get an iterator at the end of the slots
+        void      set_limit(size_t _count) { m_limit = _count; }  // Set a slot count limit
+        size_t    get_limit()const { return m_limit; }            // Get the slot count limit
+        T         back() { return m_slots.back(); }               // Get the last slot (check if not empty() before)
+        bool      empty() const { return m_slots.empty(); }       // Check if slots are empty
+        size_t    size() const { return m_slots.size(); }         // Get the slot count
+        T         operator[](size_t _index) const { return m_slots[_index]; } // Get the element at zero-based index
+        bool      accepts() const { return m_slots.size() < m_limit; }        // Check if accepts new elements (depends on limit)
 
-        /** Remove a given node */
-        void remove(T _node)
+        void remove(T item)                                                   // Remove a given item from the slots
         {
-            auto found = std::find(m_slots.begin(), m_slots.end(), _node);
+            auto found = std::find(m_slots.begin(), m_slots.end(), item);
             m_slots.erase(found);
-            m_on_removed.emit(_node);
+            m_on_removed.emit(item);
         }
 
-        /** Add a given node */
-        void add(T _node)
+        void add(T item)                                                     // Add a given item to the slots
         {
             NDBL_ASSERT(m_slots.size() < m_limit);
-            m_slots.push_back(_node);
-            m_on_added.emit(_node);
+            m_slots.push_back(item);
+            m_on_added.emit(item);
         }
 
-        /** Get a pointer to the first slot or nullptr if empty */
-        T get_front_or_nullptr() const
+        T get_front_or_nullptr() const                                        // Get a pointer to the first item or nullptr if empty
         {
             return m_slots.empty() ? nullptr : m_slots[0];
         }
 
-        /** Get a pointer to the last slot or nullptr if empty */
-        T get_back_or_nullptr() const
+        T get_back_or_nullptr() const                                         // Get a pointer to the last item or nullptr if empty
         {
             return m_slots.empty() ? nullptr : m_slots.back();
         }
 
-        /** Triggered when a node is added */
-        observe::Event <T> m_on_added;
-        /** Triggered when a node is removed */
-        observe::Event <T> m_on_removed;
+        observe::Event <T> m_on_added;                                       // Triggered when an item is added
+        observe::Event <T> m_on_removed;                                     // Triggered when an item is removed
 
     private:
-        T               m_parent;
-        size_t          m_limit;
-        std::vector <T> m_slots;
+        size_t          m_limit;                                             // maximum item count
+        std::vector <T> m_slots;                                             // item container
     };
 }
