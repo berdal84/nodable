2c956a747b43f8ecb714fc0736a0fc7ac1bae216
berdal84 <berenger@42borgata.com>
2023-01-19 00:38:52 -0500

    refactor(Component): comments

--- src/core/include/nodable/core/Component.h
+++ src/core/include/nodable/core/Component.h
@@ -2,8 +2,9 @@
 
 #include <nodable/core/reflection/reflection>
 
-namespace ndbl{
-
+namespace ndbl
+{
+    // forward declaration
     class Node;
 
     /**
@@ -11,9 +12,11 @@ namespace ndbl{
      */
 	class Component
 	{
-        friend Node;
+        friend Node; // tightly coupled
     public:
-
+        Component(): m_owner(nullptr) {}
+        Component (const Component&) = delete;                    // disable to avoid mistakes
+        Component& operator= (const Component&) = delete;         //       ... same ...
         virtual bool    update() = 0;                             // Called each frame
         inline Node*    get_owner()const { return m_owner; }      // Get the node owning this component
         template<class T> [[nodiscard]]                           // Shorthand to cast<T>(this)
@@ -22,15 +25,10 @@ namespace ndbl{
             inline const T* as()const { return cast<const T>(this); }
 
 	protected:
-		Component() = default;
-		virtual ~Component() = default;
-        Component (const Component&) = delete;
-        Component& operator= (const Component&) = delete;
-        /** Set the node owning this component */
-		virtual void  set_owner(Node* node){ m_owner = node; }
+        virtual void  set_owner(Node* node){ m_owner = node; }     // Set the component's owner
 
-	private:
-		Node* m_owner = nullptr;
+        Node* m_owner;                                             // The Node which own this component
+
 		REFLECT_BASE_CLASS()
     };
 }
