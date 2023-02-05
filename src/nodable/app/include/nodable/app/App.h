69166ad3f2357d4a16f126fb3f7fecab4ada25f0
berdal84 <berenger@42borgata.com>
2023-02-05 12:20:47 -0500

    refactor(ndbl::App): clean header

--- src/nodable/app/include/nodable/app/App.h
+++ src/nodable/app/include/nodable/app/App.h
@@ -22,14 +22,13 @@ namespace ndbl
     class App : public fw::App
 	{
 	public:
-        using Files = std::vector<File*>;
-
 		App();
 
+    protected:
 		bool            onInit() override;
 		void            onShutdown() override;
 		void            onUpdate() override;
-
+    public:
         // IAppCtx implementation
         bool            open_file(const fs_path& _filePath);
         File*           new_file();
@@ -38,7 +37,7 @@ namespace ndbl
         void            close_file(File*);
         bool            is_current(const File* _file ) const { return m_current_file == _file; }
         void            current_file(File *_file);
-        const Files&    get_files() const { return m_loaded_files; }
+        const std::vector<File*>& get_files() const { return m_loaded_files; }
         bool            has_files() const { return !m_loaded_files.empty(); }
         File*           current_file()const;
         void            run_program();
@@ -47,14 +46,12 @@ namespace ndbl
         void            stop_program();
         void            reset_program();
         bool            compile_and_load_program();
-        void            handle_events();
         static App&     get_instance();
 
     private:
         File*           m_current_file;
         VirtualMachine  m_vm;
         size_t          m_current_file_index;
-        Files           m_loaded_files;
-
+        std::vector<File*> m_loaded_files;
     };
 }
