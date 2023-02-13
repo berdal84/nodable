dd78498adb56baf5d94a01f4c7ca73d79023204f
Bérenger DALLE-CORT <berenger@42borgata.com>
2023-02-12 20:47:19 -0500

    refactor(fw/FontConf): comments

--- projects/framework/gui/include/fw/gui/FontConf.h
+++ projects/framework/gui/include/fw/gui/FontConf.h
@@ -1,13 +1,10 @@
 #pragma once
 
-/**
- * A simple struct to store font configuration
- */
-struct FontConf
+struct FontConf              // Struct to store font configuration
 {
-    const char*  id;//Font identifier
-    const char*  path; // Font path relative to application folder
-    float        size; // Font size in px
+    const char*  id;           // Font identifier
+    const char*  path;         // Font path relative to application folder
+    float        size;         // Font size in px
     bool         icons_enable; // If true, icons will be merged to the font
-    float        icons_size;
+    float        icons_size;   // If icons_enable is true, this will define icons size
 };
