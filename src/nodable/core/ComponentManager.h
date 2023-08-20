f7cf1f824dbc3278029b795984a3a556d137d7f3
berdal84 <berenger@42borgata.com>
2023-08-20 15:46:42 -0400

    refactor(ComponentManager): extract inline implementations

--- src/nodable/core/ComponentManager.h
+++ src/nodable/core/ComponentManager.h
@@ -3,14 +3,10 @@
 #include <unordered_set>
 #include "Node.h"
 
-namespace ndbl {
-
+namespace ndbl
+{
     // forward declarations
     class Component;
-    class InvokableComponent;
-    class NodeView;
-    class Physics;
-    class Scope;
 
     /**
      * Static class responsible for create/destroy/attach/remove components
@@ -48,47 +44,7 @@ namespace ndbl {
         //
         static std::unordered_set<Component*>              s_components;
         static std::unordered_multimap<size_t, Component*> s_components_by_type;
-     };
-
-    template<class ComponentT, typename ...Args>
-    ComponentT* ComponentManager::create(Args... args)
-    {
-        ComponentT* component = new ComponentT(args...);
-        s_components.insert(component);
-        return component;
-    }
-
-    template<class T>
-    std::vector<T*> ComponentManager::collect(const std::vector<Node*>& nodes)
-    {
-        std::vector<T*> components_found;
-
-        auto get_component_if_exists = [&components_found](const Node* node) -> void
-        {
-            if (T* view = node->components.get<T>())
-                components_found.push_back(view);
-        };
-
-        components_found.reserve(nodes.size()); // we anticipate the worst case scenario when each node has the expected component
-        std::for_each(nodes.begin(), nodes.end(), get_component_if_exists);
-
-        return components_found; // wil be moved
-    }
-
-    template<class... Types>
-    void ComponentManager::init_for()
-    {
-        auto types = fw::type::get_all<Types...>::types();
-        LOG_MESSAGE("ComponentManager", "Initializing for ...\n");
-
-        for(const fw::type* each_type : types )
-        {
-            LOG_MESSAGE("ComponentManager", "- %s\n", each_type->get_name() );
-
-            // TODO: instantiate N pools (one per Type)
-            //       each pool will be responsible for storing a given Type contiguously in memory.
-        }
-
-        LOG_MESSAGE("ComponentManager", "Initializing DONE.\n");
-    }
+    };
 }
+
+#include "ComponentManager.inl"
