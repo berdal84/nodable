5a196ae7aa595ff0eb04708712077da995d81f27
berdal84 <berenger@42borgata.com>
2023-09-29 23:47:07 -0400

    fix(Node.specs)

--- src/nodable/core/Node.specs.cpp
+++ src/nodable/core/Node.specs.cpp
@@ -27,7 +27,7 @@ TEST_F(Node_, get_parent)
     graph.connect(
             child->find_slot( THIS_PROPERTY, SlotFlag_PARENT ),
             parent->find_slot( THIS_PROPERTY, SlotFlag_CHILD ),
-            ConnectFlag_::OFF);
+            ConnectFlag_NONE);
 
     EXPECT_TRUE( child->get_parent() );
     EXPECT_EQ( child->get_parent(), parent );
