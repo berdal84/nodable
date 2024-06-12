bae6665a0714215bf36c0da1bb3fcdbe7cd48121
Bérenger Dalle-Cort <berenger@42borgata.com>
2024-06-12 14:39:36 -0400

    fix(ndbl): Register::undefined

--- src/ndbl/core/assembly/Register.h
+++ src/ndbl/core/assembly/Register.h
@@ -9,11 +9,11 @@ namespace ndbl::assembly
      * @see https://www.cs.uaf.edu/2017/fall/cs301/reference/x86_64.html
      */
     enum Register : u8_t {
-        undefined,  // undefined is the default value
-        rax,        // primary accumulator
-        rdx,        // storage
-        eip,        // The instruction pointer.
-        COUNT
+        rax = 0,        // primary accumulator
+        rdx = 1,        // storage
+        eip = 2,        // The instruction pointer.
+        COUNT,
+        undefined = std::numeric_limits<u8_t>::max(),  // undefined is the default value
     };
 
     R_ENUM(Register)
