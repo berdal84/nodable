e8d843636f560dd5c6e686e373e3faa8e90562ce
Bérenger Dalle-Cort <berenger@42borgata.com>
2024-10-25 00:01:50 -0400

    fix(DirectedEdge): not => !

--- src/ndbl/core/DirectedEdge.h
+++ src/ndbl/core/DirectedEdge.h
@@ -15,7 +15,7 @@ namespace ndbl
 
         inline DirectedEdge& operator=(const DirectedEdge& other) { tail = other.tail; head = other.head; return *this;}
         inline               operator bool () const { return tail != nullptr && head != nullptr; }
-        inline bool          operator!=( const DirectedEdge& other ) const { return not(*this == other); }
+        inline bool          operator!=( const DirectedEdge& other ) const { return !(*this == other); }
         inline bool          operator==( const DirectedEdge &other ) const { return tail == other.tail && head == other.head; }
         inline SlotFlags     type() const { return tail->type(); /* both tail and head share the same type */ }
     };
