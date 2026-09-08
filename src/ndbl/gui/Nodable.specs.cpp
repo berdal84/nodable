3f13dd93ebf0d6599239fa1c71d1e38bdcaca9d4
Bérenger Dalle-Cort <berenger@42borgata.com>
2026-09-07 21:23:13 -0400

    call do_fram in tests to get temp buffer reset

--- src/ndbl/gui/Nodable.specs.cpp
+++ src/ndbl/gui/Nodable.specs.cpp
@@ -27,14 +27,14 @@ TEST_F(Gui_App, update)
 TEST_F(Gui_App, loop_count_1)
 {
     ndbl::app_init();
-    loop_count(1);
+    loop_for_n_frame(1);
     ndbl::app_shutdown();
 }
 
 TEST_F(Gui_App, loop_duration_5s)
 {
     ndbl::app_init();
-    loop_for_x_sec(5.0 );
+    loop_for_n_sec(5.0 );
     ndbl::app_shutdown();
 }
 
@@ -43,7 +43,7 @@ TEST_F(Gui_App, new_file)
     ndbl::app_init();
     ndbl::appview_show_splashscreen(false);
     ndbl::app_new_file();
-    loop_for_x_sec(1.0 );
+    loop_for_n_sec(1.0 );
     ndbl::appview_save_screenshot("TEST_Gui_ndbl::app__new_file__0.png");
     ndbl::app_shutdown();
 }
@@ -53,10 +53,10 @@ TEST_F(Gui_App, open_file)
     ndbl::App_State app;
     ndbl::app_init();
     ndbl::appview_show_splashscreen(false);
-    loop_for_x_sec(1.0 );
+    loop_for_n_sec(1.0 );
     appview_save_screenshot("TEST_Gui_ndbl::app__open_file__0.png");
     EXPECT_TRUE(ndbl::app_open_asset_file("examples/arithmetic.cpp"));
-    loop_for_x_sec(1.0 );
+    loop_for_n_sec(1.0 );
     appview_save_screenshot("TEST_Gui_ndbl::app__open_file__1.png");
     ndbl::app_shutdown();
 }
@@ -67,10 +67,10 @@ TEST_F(Gui_App, close_file)
     ndbl::app_init();
     ndbl::appview_show_splashscreen(false);
     File* file = ndbl::app_open_asset_file("examples/arithmetic.cpp");
-    loop_for_x_sec(1.0 );
+    loop_for_n_sec(1.0 );
     appview_save_screenshot("TEST_Gui_ndbl::app__close_file__0.png");
     ndbl::app_close_file(file );
-    loop_for_x_sec(1.0 );
+    loop_for_n_sec(1.0 );
     appview_save_screenshot("TEST_Gui_ndbl::app__close_file__1.png");
     ndbl::app_shutdown();
 }
@@ -83,16 +83,16 @@ TEST_F(Gui_App, open_examples)
     ndbl::app_update();
     appview_save_screenshot("TEST_Gui_ndbl::app__open_examples__0.png");
     EXPECT_TRUE(ndbl::app_open_asset_file("examples/arithmetic.cpp"));
-    loop_for_x_sec(1.0 );
+    loop_for_n_sec(1.0 );
     appview_save_screenshot("TEST_Gui_ndbl::app__open_examples__1.png");
     EXPECT_TRUE(ndbl::app_open_asset_file("examples/for-loop.cpp"));
-    loop_for_x_sec(1.0 );
+    loop_for_n_sec(1.0 );
     appview_save_screenshot("TEST_Gui_ndbl::app__open_examples__2.png");
     EXPECT_TRUE(ndbl::app_open_asset_file("examples/if-else.cpp"));
-    loop_for_x_sec(1.0 );
+    loop_for_n_sec(1.0 );
     appview_save_screenshot("TEST_Gui_ndbl::app__open_examples__3.png");
     EXPECT_TRUE(ndbl::app_open_asset_file("examples/multi-instructions.cpp"));
-    loop_for_x_sec(1.0 );
+    loop_for_n_sec(1.0 );
     appview_save_screenshot("TEST_Gui_ndbl::app__open_examples__4.png");
     ndbl::app_shutdown();
 }
--- src/ndbl/gui/fixtures/gui.h
+++ src/ndbl/gui/fixtures/gui.h
@@ -29,15 +29,11 @@ namespace testing
             memory_manager_shutdown();
         }
         
-        /**
-         * run some loops for a given iteration count
-         */
-        static void loop_count(size_t iteration_count, double sleep_in_sec = 1.0)
+        static void loop_for_n_frame(size_t frame_count, double sleep_in_sec = 1.0)
         {
-            for(size_t i = 0; i < iteration_count; ++i)
+            for(size_t i = 0; i < frame_count; ++i)
             {
-                EXPECT_NO_THROW(ndbl::app_update());
-                EXPECT_NO_THROW(ndbl::app_draw());
+                EXPECT_NO_THROW(ndbl::app_do_frame());
             }
             SLEEP_FOR_HUMAN((long)(1000.0 * sleep_in_sec));
         }
@@ -45,21 +41,18 @@ namespace testing
         /**
          * run some loops for a given duration
          */
-        static void loop_for_x_sec(double duration_in_sec)
+        static void loop_for_n_sec(double total_duration_in_sec)
         {
-            auto start = std::chrono::system_clock::now();
-            auto end   = std::chrono::system_clock::now();
-            size_t iteration = 0;
-            while((std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() < (long long)(duration_in_sec * 1000.f)))
+            auto   start = std::chrono::system_clock::now();
+            auto   end   = std::chrono::system_clock::now();
+            size_t frame = 0;
+            while((std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() < (long long)(total_duration_in_sec * 1000.f)))
             {
                 end = std::chrono::system_clock::now();
-                TOOLS_LOG(tools::Verbosity_Message, "Test", "Loop iteration %llu (time: %0.1f/%0.1f sec)\n"
-                , iteration
-                , float(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()) / 1000.0f
-                , duration_in_sec);
-                EXPECT_NO_THROW(ndbl::app_update());
-                EXPECT_NO_THROW(ndbl::app_draw());
-                ++iteration;
+                float elapsed_in_sec = float(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()) / 1000.0f;
+                TOOLS_LOG(Verbosity_Message, "Test", "Loop frame %llu (time: %0.1f/%0.1f sec)\n", frame, elapsed_in_sec, total_duration_in_sec);
+                EXPECT_NO_THROW(ndbl::app_do_frame());
+                ++frame;
             }
         }
     };
