3ec0f454d47f88089b985424204d5c6244369a0c
Bérenger Dalle-Cort <berenger@42borgata.com>
2026-09-10 21:50:38 -0400

    refactor bdc::Array API, shorter template symbols

--- src/bdc/Array.hpp
+++ src/bdc/Array.hpp
@@ -11,47 +11,47 @@
 
 namespace bdc
 {
-    template<typename _Elem_Type>
+    template<typename _Elem_T>
     struct Array
     {
-        using Elem_Type = _Elem_Type;
+        using Elem_T = _Elem_T;
 
-        u32_t      size; // size must be 1st to be the same type as Inlined_Array
-        Elem_Type* data;
+        u32_t   size; // size must be 1st to be the same type as Inlined_Array
+        Elem_T* data;
 
         Array()             = default;
         Array(const Array&) = default;
         ~Array()            = default;
 
-        Array(u32_t _size, Elem_Type* _data): data(_data), size(_size) {}
-        Array(const std::initializer_list<Elem_Type>& list)
-        : data(const_cast<Elem_Type*>(&*list.begin()) )
+        Array(u32_t _size, Elem_T* _data): data(_data), size(_size) {}
+        Array(const std::initializer_list<Elem_T>& list)
+        : data(const_cast<Elem_T*>(&*list.begin()) )
         , size( list.size() )
         {}
 
-        inline const Elem_Type& operator[](u32_t pos) const { assert(pos < size && "out of bounds");return data[pos]; }
-        inline Elem_Type&       operator[](u32_t pos)       { assert(pos < size && "out of bounds");return data[pos]; }
-        inline Elem_Type*       begin()        { return data; }
-        inline Elem_Type*       end()          { return data + size; }
-        inline const Elem_Type* begin() const { return data; }
-        inline const Elem_Type* end() const   { return data + size; }
+        inline const Elem_T& operator[](u32_t pos) const { assert(pos < size && "out of bounds");return data[pos]; }
+        inline Elem_T&       operator[](u32_t pos)       { assert(pos < size && "out of bounds");return data[pos]; }
+        inline Elem_T*       begin()        { return data; }
+        inline Elem_T*       end()          { return data + size; }
+        inline const Elem_T* begin() const { return data; }
+        inline const Elem_T* end() const   { return data + size; }
     };
 
     // Simply declare array_join, user must implement it. Only array_join for String is implement in String.hpp.
-    template<typename Elem_Type>
-    Elem_Type array_join(const Array<Elem_Type>& array, const Elem_Type& separator );
+    template<typename Elem_T>
+    Elem_T array_join(const Array<Elem_T>& array, const Elem_T& separator );
 
     
     //
     // Resizable_Array<T> is like a Array<T> memory wise, but is aware of its buffer capacity and allocator.
     //
-    template<typename _Elem_Type>
+    template<typename _Elem_T>
     struct Resizable_Array
     {
-        using Elem_Type = _Elem_Type;
+        using Elem_T = _Elem_T;
 
         u32_t       size = 0; // size must be 1st to be the same type as Inlined_Array
-        Elem_Type*  data = 0;
+        Elem_T*     data = 0;
         u32_t       capacity = 0;
         Allocator*  allocator = 0;
             
@@ -59,38 +59,38 @@ namespace bdc
         Resizable_Array(const Resizable_Array& ) = default;
         ~Resizable_Array() = default;
 
-        inline const Elem_Type& operator[](u32_t pos) const { assert(pos < size && "out of bounds");return data[pos]; }
-        inline Elem_Type&       operator[](u32_t pos)       { assert(pos < size && "out of bounds");return data[pos]; }
-        inline Elem_Type*       begin()        { return data; }
-        inline Elem_Type*       end()          { return data + size; }
-        inline const Elem_Type* begin() const { return data; }
-        inline const Elem_Type* end() const   { return data + size; }
+        inline const Elem_T& operator[](u32_t pos) const { assert(pos < size && "out of bounds");return data[pos]; }
+        inline Elem_T&       operator[](u32_t pos)       { assert(pos < size && "out of bounds");return data[pos]; }
+        inline Elem_T*       begin()        { return data; }
+        inline Elem_T*       end()          { return data + size; }
+        inline const Elem_T* begin() const { return data; }
+        inline const Elem_T* end() const   { return data + size; }
     };
 
     //
     // Inlined_Array is like a fixed-capacity Array<T> that is aware of its (fixed-)capacity.
     //
-    template<typename _Elem_Type, u32_t CAPACITY>
+    template<typename _Elem_T, u32_t CAPACITY>
     struct Inlined_Array
     {
-        using Elem_Type = _Elem_Type;
+        using Elem_T = _Elem_T;
 
         u32_t     size = 0;
-        Elem_Type data[CAPACITY]; // data must be 2nd to match with other arrays
+        Elem_T    data[CAPACITY]; // data must be 2nd to match with other arrays
 
         constexpr u32_t capacity() const
         { return CAPACITY; }
 
-        inline const Elem_Type& operator[](u32_t pos) const { assert(pos < size && "out of bounds");return data[pos]; }
-        inline Elem_Type&       operator[](u32_t pos)       { assert(pos < size && "out of bounds");return data[pos]; }
-        inline Elem_Type*       begin()        { return data; }
-        inline Elem_Type*       end()          { return data + size; }
-        inline const Elem_Type* begin() const { return data; }
-        inline const Elem_Type* end() const   { return data + size; }
+        inline const Elem_T& operator[](u32_t pos) const { assert(pos < size && "out of bounds");return data[pos]; }
+        inline Elem_T&       operator[](u32_t pos)       { assert(pos < size && "out of bounds");return data[pos]; }
+        inline Elem_T*       begin()        { return data; }
+        inline Elem_T*       end()          { return data + size; }
+        inline const Elem_T* begin() const { return data; }
+        inline const Elem_T* end() const   { return data + size; }
     };
     static_assert( std::is_default_constructible_v<Inlined_Array<i8_t, 16>> );
     
-    // Concept to know if a give type is compatible with the generic array API
+    // Minimal concept to know if a given type is compatible with the generic array API
     template<typename T>
     concept Is_Array = requires(T& t, u32_t pos)
     {
@@ -98,30 +98,30 @@ namespace bdc
         t.data;
     };
     
-    // Generic Array API (works on both Array and Resizable_Array)
+    // Generic Array API (works on Array, Inline_Array and Resizable_Array)
 
-    template<Is_Array Array_Type, typename Elem_Type = Array_Type::Elem_Type>
-    inline Array<Elem_Type> array_view(const Array_Type& arr)
+    template<Is_Array Array_Type, typename Elem_T = Array_Type::Elem_T>
+    inline Array<Elem_T> array_view(const Array_Type& arr)
     {
-        return Array<Elem_Type>( arr.size, (Elem_Type*)arr.data );
+        return Array<Elem_T>( arr.size, (Elem_T*)arr.data );
     }
 
-    template<Is_Array Array_Type, typename Elem_Type = Array_Type::Elem_Type>
-    inline Elem_Type& array_front(Array_Type& arr)
+    template<Is_Array T>
+    inline T::Elem_T& array_front(T& arr)
     {
         assert(arr.size);
         return arr[0];
     }
 
-    template<Is_Array Array_Type, typename Elem_Type = Array_Type::Elem_Type>
-    inline Elem_Type& array_back(Array_Type& arr)
+    template<Is_Array T>
+    inline T::Elem_T& array_back(T& arr)
     {
         assert(arr.size);
         return arr[arr.size-1];
     }
 
-    template<Is_Array Array_Type, typename Elem_Type = Array_Type::Elem_Type>
-    inline const Elem_Type& array_back(const Array_Type& arr)
+    template<Is_Array T>
+    inline const T::Elem_T& array_back(const T& arr)
     {
         assert(arr.size);
         return arr[arr.size-1];
@@ -133,8 +133,8 @@ namespace bdc
         u32_t at_pos = (u32_t)-1;
     };
 
-    template<Is_Array Array_Type, typename Elem_Type = Array_Type::Elem_Type>
-    Array_Find_Result array_find(Array_Type& arr, const Elem_Type& elem)
+    template<Is_Array T>
+    Array_Find_Result array_find(T& arr, const typename T::Elem_T& elem)
     {
         for(u32_t i = 0; i < arr.size; i++)
             if ( arr[i] == elem )
@@ -143,8 +143,8 @@ namespace bdc
         return { .found = false };
     }
 
-    template<Is_Array Array_Type, typename Elem_Type = Array_Type::Elem_Type>
-    Array_Find_Result array_rfind(Array_Type& arr, const Elem_Type& elem)
+    template<Is_Array T>
+    Array_Find_Result array_rfind(T& arr, const typename T::Elem_T& elem)
     {
         if( arr.size == 0)
             return { .found = false };
@@ -155,9 +155,17 @@ namespace bdc
 
         return { .found = false };
     }
+    
+    template<Is_Array T>
+    Ref_Or_Ptr<typename T::Elem_T> array_append(T& arr, Ref_Or_Ptr<const typename T::Elem_T> elem)
+    {
+        array_resize(arr, arr.size + 1 );
+        arr[arr.size-1] = elem;
+        return arr[arr.size-1];
+    }
 
-    template<Is_Array Array_Type>
-    void array_remove_ordered(Array_Type& arr, u32_t pos)
+    template<Is_Array T>
+    void array_remove_ordered(T& arr, u32_t pos)
     {
         assert( pos <= arr.size );
 
@@ -167,41 +175,32 @@ namespace bdc
         arr.size -= 1;
     }
 
-    // Inlined_Array API
+    template<Is_Array T>
+    void array_remove_unordered(T& arr, u32_t pos)
+    {
+        static_assert(false, "array_remove_unordered is not implemented yet");
+    }
 
-    template<typename Elem_Type, u32_t CAPACITY>
-    void array_init(Inlined_Array<Elem_Type, CAPACITY>& arr, u32_t initial_size = 0)
+    // Inlined_Array specific API
+
+    template<typename Elem_T, u32_t CAPACITY>
+    void array_init(Inlined_Array<Elem_T, CAPACITY>& arr, u32_t initial_size = 0)
     {
         if ( initial_size )
             array_resize(arr, initial_size);
     }
 
-    template<typename Elem_Type, u32_t CAPACITY>
-    void array_resize(Inlined_Array<Elem_Type, CAPACITY>& arr, u32_t new_size)
+    template<typename Elem_T, u32_t CAPACITY>
+    void array_resize(Inlined_Array<Elem_T, CAPACITY>& arr, u32_t new_size)
     {
         assert( new_size <= arr.capacity() );
         arr.size = new_size;
     }
 
-    template<typename Type>
-    using As_Ref = std::conditional_t<
-        std::is_pointer_v<Type>,
-        std::remove_pointer_t<Type>*,
-        Type&
-    >;
+    // Resizable_Array specific API
 
-    template<typename Elem_Type, u32_t CAPACITY>
-    As_Ref<Elem_Type> array_append(Inlined_Array<Elem_Type, CAPACITY>& arr, As_Ref<const Elem_Type> elem)
-    {
-        array_resize(arr, arr.size + 1 );
-        arr[arr.size-1] = elem;
-        return arr[arr.size-1];
-    }
-
-    // Resizable_Array API
-
-    template<typename Elem_Type>
-    void array_init(Resizable_Array<Elem_Type>& arr, u32_t initial_capacity = 0, Allocator* _allocator = nullptr)
+    template<typename Elem_T>
+    void array_init(Resizable_Array<Elem_T>& arr, u32_t initial_capacity = 0, Allocator* _allocator = nullptr)
     {
         arr.size        = 0;
         arr.data        = nullptr;
@@ -210,8 +209,8 @@ namespace bdc
         array_ensure_has_capacity(arr, initial_capacity);
     }
 
-    template<typename Elem_Type>
-    void array_release(Resizable_Array<Elem_Type>& arr)
+    template<typename Elem_T>
+    void array_release(Resizable_Array<Elem_T>& arr)
     {
         if(arr.data == nullptr)
         {
@@ -235,8 +234,8 @@ namespace bdc
         return capacity;
     }
 
-    template<typename Elem_Type>
-    void array_resize(Resizable_Array<Elem_Type>& arr, u32_t new_size)
+    template<typename Elem_T>
+    void array_resize(Resizable_Array<Elem_T>& arr, u32_t new_size)
     {
         if( arr.capacity < new_size )
         {
@@ -246,13 +245,13 @@ namespace bdc
 
         for( u32_t i = arr.size; i < new_size; ++i)
         {
-            new (arr.data + i) Elem_Type();
+            new (arr.data + i) Elem_T();
         }
         arr.size = new_size;
     }
 
-    template<typename Elem_Type>
-    void array_ensure_has_capacity(Resizable_Array<Elem_Type>& arr, u32_t capacity)
+    template<typename Elem_T>
+    void array_ensure_has_capacity(Resizable_Array<Elem_T>& arr, u32_t capacity)
     {
         if( arr.capacity >= capacity )
         {
@@ -263,35 +262,28 @@ namespace bdc
 
         if( arr.data == nullptr )
         {
-            arr.data = memory_malloc_array<Elem_Type>(capacity, arr.allocator);
+            arr.data = memory_malloc_array<Elem_T>(capacity, arr.allocator);
         }
         else
         {
-            arr.data = memory_realloc_array<Elem_Type>(arr.data, capacity, arr.allocator);
+            arr.data = memory_realloc_array<Elem_T>(arr.data, capacity, arr.allocator);
         }
         assert(arr.data != nullptr);
-        memset( (void*)(arr.data + arr.capacity), 0, (capacity - arr.capacity) * sizeof(Elem_Type)); // new elements are zero-initialized
+        memset( (void*)(arr.data + arr.capacity), 0, (capacity - arr.capacity) * sizeof(Elem_T)); // new elements are zero-initialized
         arr.capacity = capacity;
     }
 
-    template<typename Elem_Type>
-    void array_append(Resizable_Array<Elem_Type>& arr, const Elem_Type& elem)
-    {
-        array_resize( arr, arr.size + 1 );
-        arr[arr.size-1] = elem;
-    }
-
-    template<typename Elem_Type>
-    void array_append(Resizable_Array<Elem_Type>& arr, std::initializer_list<Elem_Type>&& list)
+    template<typename Elem_T>
+    void array_append(Resizable_Array<Elem_T>& arr, std::initializer_list<Elem_T>&& list)
     {
         for( auto& each : list)
             array_append(arr, each);
     }
 
-    template<typename Elem_Type>
-    Array<Elem_Type> array_concat(const Array<Elem_Type>& a, const Array<Elem_Type>& b, Allocator* allocator )
+    template<typename Elem_T>
+    Array<Elem_T> array_concat(const Array<Elem_T>& a, const Array<Elem_T>& b, Allocator* allocator )
     {
-        Resizable_Array<Elem_Type> result{};
+        Resizable_Array<Elem_T> result{};
         array_resize(result, a.size + b.size);
         memcpy( result.data   , a.data, a.size);
         memcpy(&result[a.size], b.data, b.size);
--- src/bdc/Type_Traits.hpp
+++ src/bdc/Type_Traits.hpp
@@ -2,3 +2,20 @@
 
 #include <type_traits>  // for std::is_default_constructible_v and co.
 #include <utility>      // for std::forward
+
+namespace bdc
+{
+    //
+    // Return a type referencing Type as a T& or T*
+    // ex:
+    //  std::string => std::string&
+    //  const char* => const char* 
+    //  u64_t       => u64_t&
+    //
+    template<typename Type>
+    using Ref_Or_Ptr = std::conditional_t<
+        std::is_pointer_v<Type>,
+            std::remove_pointer_t<Type>*,
+            std::remove_reference_t<Type>&
+    >;
+}
